c6a1d653e8890dd5863aa4a23ec26d36b1c2d10a
Khieu Dinh Doanh <kd03102004@gmail.com>
2025-04-24 16:08:20 +0700

    init

---
+++ laptopbin/src/main/java/com/binlaptop/laptopbin/LaptopbinApplication.java
@@ -0,0 +1,13 @@
+package com.binlaptop.laptopbin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LaptopbinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LaptopbinApplication.class, args);
+	}
+
+}
---
+++ laptopbin/src/test/java/com/binlaptop/laptopbin/LaptopbinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.binlaptop.laptopbin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LaptopbinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
