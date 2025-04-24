43d0eab7441433feeefdda9ab2cd13a00eae330a
Khieu Dinh Doanh <kd03102004@gmail.com>
2025-04-24 16:35:09 +0700

    update

---
+++ laptopbin/src/main/java/com/binlaptop/laptopbin/HelloController.java
@@ -0,0 +1,13 @@
+package com.binlaptop.laptopbin;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping("/")
+    public String index() {
+        return "Hello World with Doanhbino";
+    }
+
+}
