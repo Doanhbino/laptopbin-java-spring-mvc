74417130a47c957b1ee91c3efcb997b7b4ea82ce
Khieu Dinh Doanh <kd03102004@gmail.com>
2025-04-26 17:30:40 +0700

    updatecode

--- laptopbin/src/main/java/com/binlaptop/laptopbin/LaptopbinApplication.java
+++ laptopbin/src/main/java/com/binlaptop/laptopbin/LaptopbinApplication.java
@@ -2,12 +2,18 @@ package com.binlaptop.laptopbin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class LaptopbinApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(LaptopbinApplication.class, args);
+
+		// container
+		ApplicationContext doanhbino = SpringApplication.run(LaptopbinApplication.class, args);
+		for (String s : doanhbino.getBeanDefinitionNames()) {
+			System.out.println(s);
+		}
 	}
 
 }
---
+++ laptopbin/src/main/java/com/binlaptop/laptopbin/services/Myservice.java
@@ -0,0 +1,8 @@
+package com.binlaptop.laptopbin.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class Myservice {
+
+}
